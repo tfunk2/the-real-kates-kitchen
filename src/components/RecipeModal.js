056498a8c47f236f1a69df62b8734af38bf468db
tfunk2 <tyler.j.funk2@gmail.com>
2020-11-04 05:05:27 -0700

    styling and responsiveness changes

--- src/components/RecipeModal.js
+++ src/components/RecipeModal.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import '../stylesheets/RecipeModal.css';
-import Veggies from '../images/veggies.jpg'
+import TestImg from '../images/IMG_0580.jpg'
 import Terrazzo from '../images/blue-terrazzo-seamless.jpg'
 
 export default function RecipeModal({ clickedRecipe, setClickedRecipe }) {
     const whichImage = () => {
         switch(clickedRecipe) {
             case "Vegetable Medley":
-                return <img alt="Vegetable Medley" className="modal-img" src={Veggies}></img>;
+                return <img alt="Vegetable Medley" className="modal-img" src={TestImg}></img>;
             case "Blueberry Cereal":
-                return <img alt="Blueberry Cereal" className="modal-img" src={Veggies}></img>;
+                return <img alt="Blueberry Cereal" className="modal-img" src={TestImg}></img>;
             case "Steak and Potatoes":
-                return <img alt="Steak and Potatoes" className="modal-img" src={Veggies}></img>;
+                return <img alt="Steak and Potatoes" className="modal-img" src={TestImg}></img>;
             case "Weird Stuff":
-                return <img alt="Weird Stuff" className="modal-img" src={Veggies}></img>;
+                return <img alt="Weird Stuff" className="modal-img" src={TestImg}></img>;
             case "Fruity Jello":
                 return <img alt="Fruity Jello" className="modal-img" src={Terrazzo}></img>;
         }
