d658cc2abbe02ddd775fe3a89e6a8852b4ac439c
tfunk2 <tyler.j.funk2@gmail.com>
2020-11-04 20:16:05 -0700

    added Mom's handwriting to each tab, pretty awesome

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import '../stylesheets/NavBar.css';
+import HandwrittenMainDishes from '../images/handwritten-main-dishes-antiquewhite.png'
+import HandwrittenSideDishes from '../images/handwritten-side-dishes-antiquewhite.png'
+import HandwrittenDesserts from '../images/handwritten-desserts-antiquewhite.png'
 
 export default function NavBar({ activePage, setActivePage, clickedRecipe }) {
 
@@ -7,13 +10,13 @@ export default function NavBar({ activePage, setActivePage, clickedRecipe }) {
         <nav className="navbar-nav">
             <ul className="navbar-ul">
                 <li className={activePage === "main" && clickedRecipe !== "" ? "active-tab-dark navbar-li" : activePage === "main"  ? "active-tab navbar-li" : "navbar-li"} onClick={() => setActivePage("main")}>
-                    <h1 className="navbar-h1">Main Dishes</h1>
+                    <img src={HandwrittenMainDishes} className="handwritten-img"></img>
                 </li>
                 <li className={activePage === "side" && clickedRecipe !== "" ? "active-tab-dark navbar-li" : activePage === "side" ? "active-tab navbar-li" : "navbar-li"} onClick={() => setActivePage("side")}>
-                    <h1 className="navbar-h1">Side Dishes</h1>
+                    <img src={HandwrittenSideDishes} className="handwritten-img"></img>
                 </li>
                 <li className={activePage === "dessert" && clickedRecipe !== "" ? "active-tab-dark navbar-li" : activePage === "dessert" ? "active-tab navbar-li" : "navbar-li"} onClick={() => setActivePage("dessert")}>
-                    <h1 className="navbar-h1">Desserts</h1>
+                    <img src={HandwrittenDesserts} className="handwritten-img"></img>
                 </li>
             </ul>
         </nav>
