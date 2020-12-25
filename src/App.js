d3fbc553b4773b699eb4b1cdabbb296d0f093daa
tfunk2 <tyler.j.funk2@gmail.com>
2020-12-25 03:19:15 -0600

    Fixed the title and a few other spacing issues

--- src/App.js
+++ src/App.js
@@ -44,11 +44,11 @@ function App() {
       }
       <header className="app-header">
         <div className="header-div">
-          <h1 className={clickedRecipe === "" ? "title-container" : "title-container-darker"}>
+          <div className={clickedRecipe === "" ? "title-container" : "title-container-darker"}>
             <span className="the-real-text">The Real </span>
             <span className="kates-text">KATE'S </span>
             <span className="kitchen-text">KITCHEN</span>
-          </h1>
+          </div>
           <SearchBar 
             query={query}
             setQuery={setQuery}
