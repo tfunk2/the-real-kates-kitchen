53f42dc9861f0003590377287f63594c9bd364d9
tfunk2 <tyler.j.funk2@gmail.com>
2021-01-27 03:28:57 -0700

    changed from recipes expanding on arrow click to click anywhere on the recipe

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,41 +1,72 @@
-import React, { useState, useEffect} from 'react';
-import UpArrow from '../images/up-arrow.png'
-import DownArrow from '../images/down-arrow.png'
+import React, { useState, useEffect } from "react";
+import UpArrow from "../images/up-arrow.png";
+import DownArrow from "../images/down-arrow.png";
 
+export default function Recipe({
+  recipe,
+  ingredients,
+  instructions,
+  recipesShown,
+  handleRecipeClick,
+}) {
+  const [isRecipeExpanded, setIsRecipeExpanded] = useState(false);
 
-export default function Recipe({ recipe, ingredients, instructions, recipesShown, handleRecipeClick }) {
-    const [isRecipeExpanded, setIsRecipeExpanded] = useState(false)
+  const handleExpansion = () => {
+    setIsRecipeExpanded(!isRecipeExpanded);
+  };
 
-    const handleExpansion = () => {
-        setIsRecipeExpanded(!isRecipeExpanded)
-    }
+  return (
+    <div className="recipe-div" onClick={handleExpansion}>
+      <h1 className="dish-name">{recipe.name}</h1>
+      {isRecipeExpanded ? (
+        <></>
+      ) : (
+        <img
+          className="food-img unexpanded-img"
+          alt={recipe.name}
+          src={recipe.image}
+        ></img>
+      )}
 
-    return(
-        <div className="recipe-div">
-            <h1 className="dish-name">{recipe.name}</h1>
-            {isRecipeExpanded ? <></> : <img className="food-img unexpanded-img" alt={recipe.name} src={recipe.image}></img>}
-
-            {
-                isRecipeExpanded ? <>
-                    <div className="img-and-ingredients">
-                        <img className="food-img" alt={recipe.name} src={recipe.image}></img>
-                        <div className="ingredients-or-instructions-div">
-                            <span className="section-topper-span">Ingredients</span>
-                            <ul className="ingredients-ul">
-                                {ingredients(recipesShown[recipesShown.indexOf(recipe)].ingredients)}
-                            </ul>
-                        </div>
-                    </div>
-                    <div id="instructions" className="ingredients-or-instructions-div">
-                        <span className="section-topper-span">Instructions</span>
-                        <ul className="instructions-ul">
-                            {instructions(recipesShown[recipesShown.indexOf(recipe)].instructions)}
-                        </ul>
-                    </div>
-                    <button className="original-recipe-button" onClick={() => handleRecipeClick(recipe.name)}>Original Recipe</button>
-                </> : <></>
-            }
-            <img className="arrow-img" src={isRecipeExpanded ? UpArrow : DownArrow} onClick={handleExpansion}></img>
-        </div>
-    )
+      {isRecipeExpanded ? (
+        <>
+          <div className="img-and-ingredients">
+            <img
+              className="food-img"
+              alt={recipe.name}
+              src={recipe.image}
+            ></img>
+            <div className="ingredients-or-instructions-div">
+              <span className="section-topper-span">Ingredients</span>
+              <ul className="ingredients-ul">
+                {ingredients(
+                  recipesShown[recipesShown.indexOf(recipe)].ingredients
+                )}
+              </ul>
+            </div>
+          </div>
+          <div id="instructions" className="ingredients-or-instructions-div">
+            <span className="section-topper-span">Instructions</span>
+            <ul className="instructions-ul">
+              {instructions(
+                recipesShown[recipesShown.indexOf(recipe)].instructions
+              )}
+            </ul>
+          </div>
+          <button
+            className="original-recipe-button"
+            onClick={() => handleRecipeClick(recipe.name)}
+          >
+            Original Recipe
+          </button>
+        </>
+      ) : (
+        <></>
+      )}
+      <img
+        className="arrow-img"
+        src={isRecipeExpanded ? UpArrow : DownArrow}
+      ></img>
+    </div>
+  );
 }
